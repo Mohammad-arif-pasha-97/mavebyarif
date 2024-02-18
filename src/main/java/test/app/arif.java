3ae19b91dbbe0cdacce30223e79f1f516183d5a8
root <root@ip-172-31-22-81.ap-southeast-1.compute.internal>
2024-02-18 08:54:22 +0000

    maven project

---
+++ src/main/java/test/app/arif.java
@@ -0,0 +1,13 @@
+ package test.app;
+
+/**
+ * as-salāmu ʿalaykum wa-raḥmatu -llāhi wa-barakātuhū
+ *
+ */
+public class arif 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "as-salāmu ʿalaykum wa-raḥmatu -llāhi wa-barakātuhū ,arif bhai" );
+    }
+}
